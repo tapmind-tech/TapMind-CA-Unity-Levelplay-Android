61314ff29c59c2111ea460f18548d569ce193e6e
Ravi Bodara <ravibodara73@gmail.com>
2026-02-17 21:22:49 +0530

    1.0.7

--- customadapter-ironsource/build.gradle.kts
+++ customadapter-ironsource/build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
     id("maven-publish")
 }
 
 android {
-    namespace = "com.ironsource.adapters.custom.istapmindcustomadapter"
+    namespace = "com.ironsource.adapters.custom.tapmind_ironsource"
     compileSdk = 36
 
     defaultConfig {
@@ -18,12 +18,18 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
 
         debug {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
@@ -44,14 +50,17 @@ android {
 dependencies {
 //    implementation(libs.androidx.core.ktx)
 //    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.13.0")
+
+    testImplementation("junit:junit:4.13.2")
+
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 
 //    add("api", "com.unity3d.ads-mediation:mediation-sdk:9.3.0")
     api("com.unity3d.ads-mediation:mediation-sdk:9.3.0")
-    api("com.unity3d.ads:unity-ads:4.12.2")
+//    api("com.unity3d.ads:unity-ads:4.12.2")
 //    compileOnly("com.unity3d.ads:mediation-sdk-banner:9.3.0")
 //    compileOnly("com.unity3d.mediation:mediation-sdk-interstitial:9.3.0")
 //    compileOnly("com.unity3d.mediation:mediation-sdk-rewarded:9.3.0")
